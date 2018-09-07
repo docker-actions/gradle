5153aa1df939f2e5a2d34545b35b507790964443
Ilja Bobkevic <ilja@spotify.com>
2018-09-07 19:07:32 +0200

    Use travis env instead of versions.txt

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,21 +5,17 @@ set -Eeuo pipefail
 docker_org="${1}"
 tag="${2}"
 
-for params in $(< versions.txt); do
-  arr_param=(${params//,/ })
-  build_args=''
-  for build_arg in ${arr_param[@]}; do
-    build_args="${build_args} --build-arg ${build_arg}"
-    eval "export ${build_arg}"
-  done
-
-  sed 's/${JAVA_VERSION}/'${JAVA_VERSION}'/' Dockerfile.template > Dockerfile
-
-  image_name=''
-  if [ "x${tag}" = "xlatest" ]; then
-    image_name="${docker_org}/gradle:${tag}-java${JAVA_TAG}"
-  else
-    image_name="${docker_org}/gradle:${GRADLE_VERSION}-java${JAVA_TAG}-${tag}"
-  fi
-  docker build ${build_args} -t ${image_name} .
+build_args=''
+for ba in $BUILD_ARGS; do
+  build_args="${build_args} --build-arg ${ba}=${!ba}"
 done
+
+sed 's/${JAVA_VERSION}/'${JAVA_VERSION}'/' Dockerfile.template > Dockerfile
+
+image_name=''
+if [ "x${tag}" = "xlatest" ]; then
+  image_name="${docker_org}/gradle:${tag}-java${JAVA_TAG}"
+else
+  image_name="${docker_org}/gradle:${GRADLE_VERSION}-java${JAVA_TAG}-${tag}"
+fi
+docker build ${build_args} -t ${image_name} .
--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,19 +5,12 @@ set -Eeuo pipefail
 docker_org="${1}"
 tag="${2}"
 
-for params in $(< versions.txt); do
-  arr_param=(${params//,/ })
-  for build_arg in ${arr_param[@]}; do
-    eval "export ${build_arg}"
-  done
+sed 's/${JAVA_VERSION}/'${JAVA_VERSION}'/' Dockerfile.template > Dockerfile
 
-  sed 's/${JAVA_VERSION}/'${JAVA_VERSION}'/' Dockerfile.template > Dockerfile
-
-  image_name=''
-  if [ "x${tag}" = "xlatest" ]; then
-    image_name="${docker_org}/gradle:${tag}-java${JAVA_TAG}"
-  else
-    image_name="${docker_org}/gradle:${GRADLE_VERSION}-java${JAVA_TAG}-${tag}"
-  fi
-  docker push ${image_name}
-done
+image_name=''
+if [ "x${tag}" = "xlatest" ]; then
+  image_name="${docker_org}/gradle:${tag}-java${JAVA_TAG}"
+else
+  image_name="${docker_org}/gradle:${GRADLE_VERSION}-java${JAVA_TAG}-${tag}"
+fi
+docker push ${image_name}
